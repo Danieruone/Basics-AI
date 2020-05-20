f241a98d6bf58aea8ca7c95d42edc666db5bb1c3
Daniel <danieruone@gmail.com>
2020-05-20 17:36:21 -0500

    refactor

--- src/Maquina de estados/machine.js
+++ src/Maquina de estados/machine.js
@@ -1,52 +1,62 @@
 const machine = {
-    counter : 10,
-    array : [],
-    state: "q0",
-    transitions: {
-      q0: {
-        change: function() {
-            console.log("Estado inicial a q1")
-            this.changeState("q1");
-            this.dispatch("change")
-        }
+  counter: 10,
+  array: [],
+  state: "q0",
+  transitions: {
+    q0: {
+      change: function () {
+        console.log("Estado inicial a q1");
+        this.changeState("q1");
+        this.dispatch("change");
       },
-      q1: {
-        change () {
-            i = 0
-            while(i++ < this.counter){
-              this.array.push('a')
+    },
+    q1: {
+      change: function () {
+        ((arr, counter) => {
+          function generarate(i = 0) {
+            if (i === counter) {
+              return;
             }
-            console.log("Estado q1 a estado final q2")
-            this.changeState("q2");
-            return this.dispatch("change")
-        }
-      },
-      q2: {
-        change () {
-          i = 0
-          while(i++ < this.counter){
-            this.array.push('b')
+            arr.push("a");
+            generarate(i + 1);
           }
-          console.log('El estado final es: ', this.array.join(''))
-        }
+          generarate();
+        })(this.array, this.counter);
+        console.log("Estado q1 a estado final q2");
+        this.changeState("q2");
+        return this.dispatch("change");
       },
     },
-    dispatch(actionName) {
-      const actions = this.transitions[this.state];
-      const action = this.transitions[this.state][actionName];
-  
-      if (action) {
-        action.apply(machine);
-      } else {
-        //action is not valid for current state
-      }
+    q2: {
+      change() {
+        ((arr, counter) => {
+          function generarate(i = 0) {
+            if (i === counter) {
+              return;
+            }
+            arr.push("b");
+            generarate(i + 1);
+          }
+          generarate();
+        })(this.array, this.counter);
+        console.log("El estado final es: ", this.array.join(""));
+      },
     },
-    changeState(newState) {
-      //validate that newState actually exists
-      this.state = newState;
-    },
-  };
-  
-  let startMachine = Object.create(machine);
-  startMachine.dispatch("change");
-  
+  },
+  dispatch(actionName) {
+    const actions = this.transitions[this.state];
+    const action = this.transitions[this.state][actionName];
+    if (action) {
+      action.apply(machine);
+    } else {
+      //action is not valid for current state
+    }
+  },
+  changeState(newState) {
+    //validate that newState actually exists
+    this.state = newState;
+  },
+};
+
+let startMachine = Object.create(machine);
+startMachine.dispatch("change");
